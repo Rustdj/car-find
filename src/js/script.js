67d40705fd5c21b78a464c63cbef740fd87dfc2b
Rustdj <rustdj@icloud.com>
2021-09-05 23:49:36 +0300

    add_btn_function

--- src/js/script.js
+++ src/js/script.js
@@ -11,30 +11,68 @@ $(window).scroll(function() {
 
     // Modal
 
-    const modalTrigger = document.querySelector('[data-modal]'),
-      modal = document.querySelector('.screan'),
-      modalCloseBtn = document.querySelectorAll('[data-close]');
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.screan'),
+          secondTrigger = document.querySelector('.screan__offer'),
+          secondModal = document.querySelector('.window'),
+          secondCloseModal = document.querySelector('[data-close]'),
+          modalCloseBtn = document.querySelector('[data-close]');
 
-    function openModal() {
+
+//==============Second modal========================================
+
+    secondTrigger.addEventListener('click', () => {
+        secondModal.classList.add('show');
+        secondModal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+    });
+
+    function closeWindow() {
+        secondModal.classList.add('hide');
+        secondModal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    secondCloseModal.addEventListener('click', closeWindow);
+
+    secondModal.addEventListener('click', (e) => {
+        if (e.target === secondModal) {
+            closeWindow();
+        }
+    });
+//-------------------------------------------------------------
+
+    /*function openModal() {
         modal.classList.add('show');
         modal.classList.remove('hide');
         document.body.style.overflow = 'hidden';
         clearInterval(modalTimerId);
-    }
+    }*/
 
 
-    modalTrigger.addEventListener('click', () => {
-        modal.classList.add('show');
-        modal.classList.remove('hide');
-        document.body.style.overflow = 'hidden';  // отключает скролл при открытой модалке
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';  // отключает скролл при открытой модалке
+        });
     });
 
-
-    modalCloseBtn[0].addEventListener('click', () => {
+    function closeModal() {
         modal.classList.add('hide');
         modal.classList.remove('show');
         document.body.style.overflow = '';
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
     });
+
+
 
 });
 
