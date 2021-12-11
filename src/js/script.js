8f7bc945fe60530eb326a2c0f64d2753cd8e8c8b
Rustdj <81041717+Rustdj@users.noreply.github.com>
2021-12-11 16:49:24 +0300

    correct adapteve

--- src/js/script.js
+++ src/js/script.js
@@ -228,3 +228,12 @@ if (iconMenu) {
     });
 }
 
+
+//spoiler
+
+$(document).ready(function() {
+    $('.block__title').click(function(event) {
+        $(this).toggleClass('active').next().slideToggle(300);
+    });
+});
+
